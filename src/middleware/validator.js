62a210ad4ac447eb9bd2b36465478401c9c6e2cc
AlexanderLiu <alexanderliu2016@gmail.com>
2023-11-05 11:31:16 -0800

    make validator

---
+++ src/middleware/validator.js
@@ -0,0 +1,52 @@
+const { missingError, missingMessage } = require("../utils/strings");
+
+const newEntrySchema = {
+  body: {
+    word: "",
+    definition: "",
+    wordLanguage: "",
+    definitionLanguage: ""
+  }
+}
+
+const updateEntrySchema = {
+  params: {
+    word: "",
+  },
+  body: {
+    definition: "",
+    definitionLanguage: ""
+  }
+}
+
+function validateMissing(schema, req) {
+  const input = {};
+  const missing = [];
+  for (const location in schema) {
+    for (const key in schema[location]) {
+      const value = req[location][key];
+      input[key] = (!value) ? "" : value;
+      if (!value) {
+        missing.push(key);
+      }
+    }
+  }
+  return [missing, input];
+}
+
+function missingValidator(schema) {
+  return (req, res, next) => {
+    const [missing, input] = validateMissing(schema, req);
+    if (missing.length > 0) {
+      res.status(400).send({ error: missingError, message: missingMessage(missing), entry: input });
+    } else {
+      next();
+    }
+  }
+}
+
+module.exports = {
+  newEntrySchema,
+  updateEntrySchema,
+  missingValidator
+}
