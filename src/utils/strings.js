e27a87d7529701839a45a587c731669cc88cfcf7
AlexanderLiu <alexanderliu2016@gmail.com>
2023-11-04 20:50:40 -0700

    update messages

--- src/utils/strings.js
+++ src/utils/strings.js
@@ -5,28 +5,28 @@ function getDefinitionErrorMessage(word) {
   return `Unable to get word ${word}, it does not exist`;
 }
 function getDefinitionMessage(word) {
-  return `Successfully retrieved word ${word}`
+  return `Successfully retrieved entry for word ${word}`
 }
 
 const postDefinitionError = "Word conflict";
 function postDefinitionErrorMessage(word) {
-  return `Unable to create new definition, word ${word} it already exists`;
+  return `Unable to create new entry, word ${word} it already exists`;
 }
 const postDefinitionMessage = "Entry created successfully";
 
-const deleteDefinitionError = "Word not found";
+const deleteDefinitionError = "Word does not exist";
 function deleteDefinitionErrorMessage(word) {
-  return `Unable to delete word ${word}, it does not exist`;
+  return `Unable to delete entry for word ${word}, it does not exist`;
 }
 function deleteDefinitionMessage(word) {
-  return `Successfully delete word ${word}`;
+  return `Successfully deleted entry for word ${word}`;
 }
 
 const patchDefinitionError = "Word does not exist";
 function patchDefinitionErrorMessage(word) {
-  return `Unable to update word ${word}, it does not exist`;
+  return `Unable to update entry for word ${word}, it does not exist`;
 }
-const patchDefinitionMessage = "Successfully updated word"
+const patchDefinitionMessage = "Successfully updated entry"
 
 module.exports = {
   getLanguagesMessage,
